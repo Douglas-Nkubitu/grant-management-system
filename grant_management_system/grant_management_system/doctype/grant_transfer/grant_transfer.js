8d424419a1bfc3c9244f8503a6666e6d9c18d6b4
navariltd <nkubitudouglas@gmail.com>
2022-06-11 19:03:13 +0300

    chore: Grant Transfer

---
+++ grant_management_system/grant_management_system/doctype/grant_transfer/grant_transfer.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2022, Navari Limited and contributors
+// For license information, please see license.txt
+
+{% include 'erpnext/non_profit/grant_transfer_property_update.js' %}
+
+frappe.ui.form.on('Grant Transfer', {
+	refresh: function(frm) {
+
+	}
+});
