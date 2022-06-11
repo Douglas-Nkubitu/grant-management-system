a7752c6f203573edacf47bde228127e1adeaf0d1
navariltd <nkubitudouglas@gmail.com>
2022-06-11 19:34:16 +0300

    chore: Grant Parameter Section

---
+++ grant_management_system/grant_management_system/doctype/grant_parameter_section/grant_parameter_section.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Navari Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Grant Parameter Section', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ grant_management_system/grant_management_system/doctype/grant_parameter_section/grant_parameter_section.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Navari Limited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class GrantParameterSection(NestedSet):
+	pass
