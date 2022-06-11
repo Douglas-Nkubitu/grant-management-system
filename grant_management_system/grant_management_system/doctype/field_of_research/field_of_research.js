66cb5c67227cd105c5dbe0cbdd617ffa7c1c21d5
navariltd <nkubitudouglas@gmail.com>
2022-06-11 19:09:37 +0300

    chore: Field of Research

---
+++ grant_management_system/grant_management_system/doctype/field_of_research/field_of_research.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Navari Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Field of Research', {
+	// refresh: function(frm) {
+
+	// }
+});
