309d01ee4e98765b25fed06df25b44bdc85d5303
navariltd <nkubitudouglas@gmail.com>
2022-06-20 15:26:50 +0300

    fix: grant project summary report

--- grant_management_system/grant_management_system/report/grant_project_summary/grant_project_summary.js
+++ grant_management_system/grant_management_system/report/grant_project_summary/grant_project_summary.js
@@ -5,5 +5,40 @@
 frappe.query_reports["Grant Project Summary"] = {
 	"filters": [
 
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname": "is_active",
+			"label": __("Is Active"),
+			"fieldtype": "Select",
+			"options": "\nYes\nNo",
+			"default": "Yes",
+		},
+		{
+			"fieldname": "status",
+			"label": __("Status"),
+			"fieldtype": "Select",
+			"options": "\nOpen\nCompleted\nCancelled",
+			"default": "Open"
+		},
+		{
+			"fieldname": "project_type",
+			"label": __("Project Type"),
+			"fieldtype": "Link",
+			"options": "Project Type"
+		},
+		{
+			"fieldname": "priority",
+			"label": __("Priority"),
+			"fieldtype": "Select",
+			"options": "\nLow\nMedium\nHigh"
+		}
 	]
 };
+
