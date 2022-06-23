bba7d5eb32a4fa66548e8a5cc6b690489925e1bf
navariltd <nkubitudouglas@gmail.com>
2022-06-23 10:37:22 +0300

    chore: volunteer docs

---
+++ grant_management_system/grant_management_system/doctype/volunteer/volunteer.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2022, Navari Limited and contributors
+# For license information, please see license.txt
+
+from frappe.contacts.address_and_contact import load_address_and_contact
+from frappe.model.document import Document
+
+
+class Volunteer(Document):
+	def onload(self):
+		"""Load address and contacts in `__onload`"""
+		load_address_and_contact(self)
